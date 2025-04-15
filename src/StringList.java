779b73c978d7b7a348ffc13bfce03d1ba8ef576b
Simon Nestler <simon.nestler@thi.de>
2025-04-15 12:31:08 +0200

    suchen und ersetzen. und aus numbernode wird stringnode. und aus numberlist wird stringlist

--- src/StringList.java
+++ src/StringList.java
@@ -1,6 +1,29 @@
 public class StringList {
+    private StringNode head;
 
-    public void add(String n) {
+    public void add(String value) {
+        StringNode newNode = new StringNode(value);
 
+        if (head == null) {
+            head = newNode;
+        } else {
+            StringNode current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+    }
+
+    public String toString() {
+        String result = "Die Liste: ";
+        StringNode current = head;
+
+        while (current != null) {
+            result += current.getValue() + " ";
+            current = current.getNext();
+        }
+
+        return result.trim();
     }
 }
--- src/StringNode.java
+++ src/StringNode.java
@@ -1,3 +1,20 @@
 public class StringNode {
+    private StringNode next;
+    private String value;
 
+    public StringNode(String value) {
+        this.value = value;
+    }
+
+    public StringNode getNext() {
+        return next;
+    }
+
+    public void setNext(StringNode next) {
+        this.next = next;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
